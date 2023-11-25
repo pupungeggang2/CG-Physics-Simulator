2dd5aef375d8803e336107e49b4ce9acf8a374b8
pupungeggang289 <kyongkyong2892@gmail.com>
2023-11-26 01:39:28 +0900

    0.0.1

--- Script/FunctionProgram.js
+++ Script/FunctionProgram.js
@@ -1,17 +1,13 @@
 // Function which creates shader
 function glInit() {
     // Shader
-    let shaderVertCode = document.getElementById('ShaderVertex')
+    let shaderVertCode = document.getElementById('ShaderVertex').innerHTML
     console.log(shaderVertCode)
     shaderVert = gl.createShader(gl.VERTEX_SHADER)
     gl.shaderSource(shaderVert, shaderVertCode)
     gl.compileShader(shaderVert)
 
-    let shaderFragCode = 
-        'void main(void) {' +
-            'gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);' +
-        '}'
-
+    let shaderFragCode = document.getElementById('ShaderFragment').innerHTML
     shaderFrag = gl.createShader(gl.FRAGMENT_SHADER)
     gl.shaderSource(shaderFrag, shaderFragCode)
     gl.compileShader(shaderFrag)
