413b63e5552701c01344a8006a5367e17a8691f5
pupungeggang2 <kyongkyong2892@gmail.com>
2023-11-28 22:04:38 +0900

    0.0.2

--- Script/FunctionPhysics.js
+++ Script/FunctionPhysics.js
@@ -6,6 +6,10 @@ function pointInsideRectArray(x, y, rect) {
     return x > rect[0] && x < rect[0] + rect[2] && y > rect[1] && y < rect[1] + rect[3]
 }
 
+function collisionCheck() {
+    
+}
+
 function moveObject() {
     
 }
